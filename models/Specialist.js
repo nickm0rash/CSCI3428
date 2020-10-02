3ef8e10cf1e9eb3ff1ec6adb327da58d7f60cb48
just1ngray <just1ngray@outlook.com>
2020-10-01 23:56:00 -0300

    Added DB Models for app structure

---
+++ models/Specialist.js
@@ -0,0 +1,19 @@
+/**
+ * Specialist is the child of Account (1:1)
+ * @author Justin Gray (A00426753)
+ */
+const mongoose = require('mongoose');
+
+module.exports = mongoose.model('Specialist', new mongoose.Schema({
+    // _id set automatically
+    // __v set automatically
+    account: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Account',
+        required: true
+    },
+    manages: [{
+        type: mongoose.Types.ObjectId,
+        ref: 'Student'
+    }]
+}));
